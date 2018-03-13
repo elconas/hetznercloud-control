684194cb14c90c6725b13703610a19a25395978e
Robert Heinzmann <reg@elconas.de>
2018-03-13 20:28:01 +0100

    Remove rpcbind

--- modules/profile/manifests/hardening.pp
+++ modules/profile/manifests/hardening.pp
@@ -2,14 +2,20 @@
 class profile::hardening (
   $ipv6 = true,
 ) {
-  class { 'os_hardening': 
+  class { 'os_hardening':
     enable_ipv6 => $ipv6,
   }
 
-  class { 'ssh_hardening': 
+  class { 'ssh_hardening':
     allow_root_with_key => true,
     ipv6_enabled => $ipv6,
     use_pam => true,
   }
-}
 
+  [ 'rpcbind' ].each |String $pkg| {
+    package { $pkg:
+      ensure => absent,
+    }
+  }
+
+}
