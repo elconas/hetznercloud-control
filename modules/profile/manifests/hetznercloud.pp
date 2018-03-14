d74879331f7ec84d9d7bc8a8c9fa98dcf6710c83
Robert Heinzmann <reg@elconas.de>
2018-03-14 15:53:35 +0100

    Add Password Protected Grub

--- modules/profile/manifests/hetznercloud.pp
+++ modules/profile/manifests/hetznercloud.pp
@@ -8,7 +8,8 @@ class profile::hetznercloud (
   include ::profile::hetznercloud::kernel
   include ::profile::hetznercloud::cloud_init
   include ::profile::hetznercloud::qemu_gest_agent
-
+  include ::profile::hetznercloud::grub
+  
   file { '/etc/rc.local':
     source  => "puppet:///modules/profile/hcloud/etc/rc.local",
     mode    => '0750',
---
+++ modules/profile/manifests/hetznercloud/grub.pp
@@ -0,0 +1,18 @@
+class profile::hetznercloud::grub (
+
+) {
+
+  ensure_resource('exec','update-grub', {
+    command     => '/usr/sbin/grub2-mkconfig --output /etc/grub2.cfg',
+    refreshonly => true,
+  })
+
+  file_line { '/etc/sysconfig/grub-GRUB2_PASSWORD':
+    ensure => present,
+    path   => '/etc/sysconfig/grub',
+    line   => 'GRUB2_PASSWORD=grub.pbkdf2.sha512.10000.A8F6537E44221968F135508800E8DDD22AC0CCE2665B649C6596E7E627864DB38D494761CF797C14109D0569ED65F14D87D55CE1D638F7D4EFBB35EAEC0CE5C8.D4736453E31920F70B1B16F2D6673C30D40096F43C05175D61E867AAE49F37C4CD3777650CFF9E3169DB0FDC839D1A0A7C62317511943942CA0CCE627B642865',
+    match  => '^GRUB2_PASSWORD=',
+    notify => Exec['update-grub'],
+  }
+
+}
